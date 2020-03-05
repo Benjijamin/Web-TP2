81106b30f288e74b885aef79afa19b3be4ceba61
sam <sam@sam.com>
2020-03-05 16:09:44 -0500

    Film

---
+++ Web-TP2/Models/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Web_TP2.Models
+{
+    public class Film
+    {
+        private int id;
+        private String nom;
+
+        public static int globalId;
+
+
+
+        public Film(String n)
+        {
+            this.Id = Interlocked.Increment(ref globalId);
+            this.Nom = n;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+
+
+
+    }
+}
